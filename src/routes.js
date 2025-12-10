ef0c192b489e180a56c6356be2f3c6d2b32745a3
Gabriel Teles <bilebile2020@gmail.com>
2025-12-10 17:06:15 -0300

    Separa header e sidebar para reusá-los no ejs; header.js contém lógica para botão do header.

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
-const { loginPage, authAdmin, create, adminPage } = require("./controllers/admin-controller");
+const { authAdmin, create } = require("./controllers/admin-controller");
 const { loginAuthMiddleware, adminAuthMiddleware } = require("./middlewares/authMiddleware");
+const { loginPage, adminPage, createProductPage } = require("./controllers/admin-render-controller");
 
 const router = Router();
+
+router.post("/create", create); // Rota manual. Incompleta.
 
 // Rotas do administrador
 router.get("/login", loginAuthMiddleware, loginPage);
 router.post("/authAdmin", authAdmin);
 router.get("/admin", adminAuthMiddleware, adminPage);
-router.post("/create", create); // Incompleta.
+router.get("/admin/create", createProductPage);
 
 module.exports = router;
